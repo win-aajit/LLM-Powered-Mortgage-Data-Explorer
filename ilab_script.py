085647d75ee353b7e36412eeeadc241cde03627c
giladbeja <78669823+giladbeja@users.noreply.github.com>
2025-05-05 18:12:57 -0400

    Update ilab_script.py

--- ilab_script.py
+++ ilab_script.py
@@ -1,27 +1,18 @@
-# ilab_script.py
-
 import sys
 import subprocess
 
-def run_query(query):
-    cmd = [
-        "psql",
-        "-h", "postgres.cs.rutgers.edu",
-        "-U", "your_netid",             # 🔁 Replace with your NetID
-        "-d", "your_database_name",     # 🔁 Replace with your database name
-        "-c", query
-    ]
-
+def run_query(query, netid):
+    cmd = ["psql","-h", "postgres.cs.rutgers.edu","-U", netid, "-d", "group31", "-c", query]
     try:
         result = subprocess.run(cmd, text=True, capture_output=True, check=True)
         print(result.stdout)
     except subprocess.CalledProcessError as e:
         print("Error executing SQL:")
         print(e.stderr)
-
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        query = sys.argv[1]
+        query = sys.argv[2]
+        netid = sys.argv[1]
     else:
         query = sys.stdin.read()
-    run_query(query)
+    run_query(query,netid)
